8e668b065f9b1144f4f2d271215030c992bc468f
Pavel Shnurov <pavel.shnurov@gmail.com>
2022-09-17 16:35:33 +0300

    ready cheakGoods

--- src/test/java/MainPage.java
+++ src/test/java/MainPage.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class MainPage {
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+/**
+ * Главная страница Яндекс Маркета
+ */
+
+
+public class MainPage {
+
+    public MainPage(String url){
+        Selenide.open(url);
+    }
+    private final SelenideElement menuButton = $x("//button[@id=\"catalogPopupButton\"]");
+    private final SelenideElement pcButton = $x("//a[@href=\"/catalog--kompiuternaia-tekhnika/54425\"]");
+    private final SelenideElement laptopButton = $x("//a[@href=\"/catalog--noutbuki-v-saratove/54544/list?hid=91013\"]");
+    private final SelenideElement manufacturerLaptopCheak = $x("//label[@data-auto=\"filter-list-item-152981\"]");
+    private final SelenideElement minPrice = $x("/html/body/div[4]/div[2]/div/div[1]/div/div[5]/div/div/div/div/div/div[2]/div/div[4]/div/div/div/div/div[1]/div/fieldset/div/div/div/span[1]/div/div/input");
+    private final SelenideElement maxPrice = $x("/html/body/div[4]/div[2]/div/div[1]/div/div[5]/div/div/div/div/div/div[2]/div/div[4]/div/div/div/div/div[1]/div/fieldset/div/div/div/span[2]/div/div/input");
+
+
+    /**
+     * выбор категории Ноутбуки
+     */
+
+
+    public void clickOnMenu() throws InterruptedException {
+        Thread.sleep(10000);
+        menuButton.click();
+        Thread.sleep(2000);
+        pcButton.click();
+        Thread.sleep(10000);
+        laptopButton.click();
+        Thread.sleep(2000);
+
+    }
+
+
+    /**
+     * выбор производителя
+     *
+     */
+
+    public void sort() throws InterruptedException {
+        manufacturerLaptopCheak.click();
+        Thread.sleep(2000);
+
+
+    }
+
+    /**
+     * сортировка по цене
+     */
+
+    public void searchMinMax(String searchString1, String searchString2)  {
+        minPrice.click();
+        minPrice.setValue(searchString1);
+        maxPrice.click();
+        maxPrice.setValue(searchString2);
+    }
+
 }
--- src/test/java/YaTest.java
+++ src/test/java/YaTest.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class YaTest {
-}
+import com.codeborne.selenide.ElementsCollection;
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.codeborne.selenide.Selenide.$$x;
+
+public class YaTest extends BaseTest {
+
+    private final static String BASE_URL = "https://market.yandex.ru/";
+    private final static String MANUFACTURER = "lenovo";
+    private final static String MIN_PRICE = "25000";
+    private final static String MAX_PRICE = "30000";
+
+    @Test
+    public void cheakGoods() throws InterruptedException {
+
+        MainPage mainPage = new MainPage(BASE_URL);
+        mainPage.clickOnMenu();
+        mainPage.sort();
+        mainPage.searchMinMax(MIN_PRICE, MAX_PRICE);
+
+
+
+        ElementsCollection hrefs = $$x("/html/body/div[4]/div[2]/div/div[1]/div/div[5]/div/div/div/div/div/div[1]/div/div[6]//a[@href]");
+        List<String> links = new ArrayList<>();
+
+        for(int i = 0; i <hrefs.size(); i++){
+            links.add(hrefs.get(i).getAttribute("href"));
+        }
+
+            for (int i = 0; i < links.size(); i++) {
+                Assert.assertTrue(links.get(i).contains(MANUFACTURER));
+            }
+
+        }
+
+    }
+
+
